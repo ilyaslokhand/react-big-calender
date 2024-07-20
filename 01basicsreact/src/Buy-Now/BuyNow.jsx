f52c38b4b772bf21a0bfdfb1889fb39a7988a3de
ilyas <ilokhandwala98@gmail.com>
2024-07-20 13:29:29 +0530

    infonas

--- 01basicsreact/src/Buy-Now/BuyNow.jsx
+++ 01basicsreact/src/Buy-Now/BuyNow.jsx
@@ -1,50 +1,99 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "./Calendar.css";
 import moment from "moment";
 import Modal from "react-modal";
+Modal.setAppElement("#root");
 
 const localizer = momentLocalizer(moment);
 
 const BuyNow = (props) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
-  const [selectedGarden, setSelectedGarden] = useState(null);
+  const [selectStartDate, setSelectStartDate] = useState("");
+  const [selectEndDate, setSelectEndDate] = useState("");
+  const [events, setEvents] = useState([]);
+  const [garden, setGarden] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [Selectedevent, setSelectedevent] = useState("");
+  const [isUpdating, setisUpdating] = useState("");
 
-  const events = [
-    {
-      start: moment("2024-07-06T12:00:00").toDate(),
-      end: moment("2024-07-06T13:00:00").toDate(),
-      title: "hello",
-    },
-  ];
+  useEffect(() => {
+    const storedEvents = JSON.parse(localStorage.getItem("events")) || [];
+    setEvents(storedEvents);
+  }, []);
 
-  const handleDateSelect = (date) => {
-    setStartDate(date);
+  useEffect(() => {
+    localStorage.setItem("events", JSON.stringify(events));
+  }, [events]);
+
+  useEffect(() => {
+    const today = new Date();
+    setEvents(events.filter((event) => new date(Event.end) >= today));
+  }, []);
+
+  const handleremoveevent = () => {
+    setEvents(events.filter((event) => event !== Selectedevent));
+    handleCloseModal();
+  };
+
+  const handleSelectSlot = (slotInfo) => {
+    setSelectStartDate(slotInfo.start);
+    setSelectEndDate(slotInfo.start);
+    setisUpdating(false);
     setModalIsOpen(true);
   };
 
-  const handleEndDateSelect = (date) => {
-    setEndDate(date);
+  const handleselectevent = (event) => {
+    setSelectedevent(event);
+    setModalIsOpen(true);
+    setEmail(event.email);
+    setGarden(event.garden);
+    setMobile(event.mobile);
+    setName(event.name);
+    setSelectStartDate(event.start);
+    setSelectEndDate(event.end);
+    setGarden(event.garden);
+    setisUpdating(true);
   };
 
-  const handleGardenSelect = (garden) => {
-    setSelectedGarden(garden);
+  const handleCloseModal = () => {
     setModalIsOpen(false);
+    setSelectedevent("");
+    setGarden("");
+    setSelectStartDate("");
+    setSelectEndDate("");
+    setName("");
+    setEmail("");
+    setMobile("");
+  };
 
-    // Logic to update events array with booked slots from startDate to endDate for selected garden
-    // Example: Update the events array with new booked slot
+  const handleGardenChange = (e) => {
+    setGarden(e.target.value);
+  };
+
+  const handleEndDateChange = (e) => {
+    setSelectEndDate(new Date(e.target.value));
+  };
+
+  const handleAddEvent = (e) => {
+    e.preventDefault();
     const newEvent = {
-      start: startDate,
-      end: endDate,
-      title: `Booked - ${selectedGarden}`, // Example title, customize as needed
+      start: selectStartDate,
+      end: selectEndDate,
+      allDay: true,
+      title: `Garden ${garden} ${name}`,
     };
-
-    const updatedEvents = [...events, newEvent];
-    // Update state or call API to update backend with new event data
-    console.log("Updated Events:", updatedEvents);
+    if (isUpdating) {
+      setEvents(
+        events.map((event) => (event === Selectedevent ? newEvent : event))
+      );
+    } else {
+      setEvents([...events, newEvent]);
+    }
+    handleCloseModal();
   };
 
   return (
@@ -54,43 +103,99 @@ const BuyNow = (props) => {
         events={events}
         startAccessor="start"
         endAccessor="end"
-        style={{ margin: 50 }}
-        onSelectSlot={handleDateSelect}
+        style={{ margin: 50, display: modalIsOpen ? "none" : "block" }}
         selectable
+        onSelectSlot={handleSelectSlot}
+        onSelectEvent={handleselectevent}
       />
       <Modal
         isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        contentLabel="Book Slot Modal"
+        onRequestClose={handleCloseModal}
+        contentLabel="Add Event"
+        className="modal-overlay"
+        ClassName="modal-overlay"
+        appElement={document.getElementById("root")}
       >
-        <h2>Select End Date</h2>
-        <input
-          type="date"
-          value={endDate}
-          onChange={(e) => handleEndDateSelect(e.target.value)}
-        />
-        <h2>Select Garden</h2>
-        <div>
-          <input
-            type="radio"
-            id="garden1"
-            name="garden"
-            value="Garden 1"
-            onChange={() => handleGardenSelect("Garden 1")}
-          />
-          <label htmlFor="garden1">Garden 1</label>
+        <div className="modal-content">
+          <div className="modal-form">
+            <h2>{Selectedevent ? "view event" : "Add event"}</h2>
+            <form onSubmit={handleAddEvent}>
+              <label>
+                Name:
+                <input
+                  type="text"
+                  value={name}
+                  placeholder="Add Name"
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </label>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  value={email}
+                  placeholder="Email Address"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </label>
+              <label>
+                Mobile Number:
+                <input
+                  type="text"
+                  value={mobile}
+                  placeholder="Mobile Number"
+                  onChange={(e) => setMobile(e.target.value)}
+                  required
+                />
+              </label>
+              <label>
+                Select Garden:
+                <select onChange={handleGardenChange} value={garden} required>
+                  <option value="">Select Garden</option>
+                  <option value="1">Garden 1</option>
+                  <option value="2">Garden 2</option>
+                  <option value="3">Garden 3</option>
+                </select>
+              </label>
+              <label>
+                End Date:
+                <input
+                  type="date"
+                  onChange={handleEndDateChange}
+                  value={
+                    selectEndDate
+                      ? moment(selectEndDate).format("YYYY-MM-DD")
+                      : ""
+                  }
+                  required
+                />
+              </label>
+              <div className="button-group">
+                <button type="submit">
+                  {isUpdating ? "Update Event" : "Add Event"}
+                </button>
+                {isUpdating && (
+                  <button
+                    type="button"
+                    className="secondary"
+                    onClick={handleremoveevent}
+                  >
+                    Remove Event
+                  </button>
+                )}
+                <button
+                  type="button"
+                  className="secondary"
+                  onClick={handleCloseModal}
+                >
+                  Close
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div>
-          <input
-            type="radio"
-            id="garden2"
-            name="garden"
-            value="Garden 2"
-            onChange={() => handleGardenSelect("Garden 2")}
-          />
-          <label htmlFor="garden2">Garden 2</label>
-        </div>
-        {/* Add more garden options as needed */}
       </Modal>
     </div>
   );
